7ff5ea8c766c1231701000cb335f5a26cb2551fc
Sahil Singh Rawat <sahilsinghrawat1405@gmail.com>
2020-07-22 09:45:16 +0530

    weather_feature

---
+++ lib/Models/Weatherdata.dart
@@ -0,0 +1,44 @@
+class WeatherData {
+  final DateTime date;
+  final String name;
+  final double temp;
+  final String main;
+  final String icon;
+
+  WeatherData({this.date, this.name, this.temp, this.main, this.icon});
+
+  factory WeatherData.fromJson(Map<String, dynamic> json) {
+    return WeatherData(
+      date: new DateTime.now(),
+      name: json['name'],
+      temp: json['main']['temp'],
+      main: json['weather'][0]['description'],
+      icon: json['weather'][0]['icon'],
+    );
+  }
+}
+
+class ForecastData {
+  final List list;
+
+  ForecastData({this.list});
+
+  factory ForecastData.fromJson(Map<String, dynamic> json) {
+    List list = new List();
+
+    for (dynamic e in json['list']) {
+      WeatherData w = new WeatherData(
+          date: new DateTime.fromMillisecondsSinceEpoch(e['dt'] * 1000,
+              isUtc: false),
+          name: json['city']['name'],
+          temp: e['main']['temp'].toDouble(),
+          main: e['weather'][0]['description'],
+          icon: e['weather'][0]['icon']);
+      list.add(w);
+    }
+
+    return ForecastData(
+      list: list,
+    );
+  }
+}
---
+++ lib/Widgets/WeatherItem.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:upajVirasat/Models/Weatherdata.dart';
+
+class Weather_1 extends StatelessWidget {
+  final WeatherData weather;
+
+  Weather_1({Key key, this.weather}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(weather.name, style: new TextStyle(color: Colors.white)),
+        Text(weather.main,
+            style: new TextStyle(color: Colors.white, fontSize: 32.0)),
+        Text('${weather.temp.toString()}°C',
+            style: new TextStyle(color: Colors.white)),
+        Image.network('https://openweathermap.org/img/w/${weather.icon}.png'),
+        Text(new DateFormat.yMMMd().format(weather.date),
+            style: new TextStyle(color: Colors.white)),
+        Text(new DateFormat.Hm().format(weather.date),
+            style: new TextStyle(color: Colors.white)),
+      ],
+    );
+  }
+}
+
+class WeatherItem extends StatelessWidget {
+  final WeatherData weather;
+
+  WeatherItem({Key key, @required this.weather}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(weather.name, style: new TextStyle(color: Colors.black)),
+            Text(weather.main,
+                style: new TextStyle(color: Colors.black, fontSize: 24.0)),
+            Text('${weather.temp.toString()}°C',
+                style: new TextStyle(color: Colors.black)),
+            Image.network(
+                'https://openweathermap.org/img/w/${weather.icon}.png'),
+            Text(new DateFormat.yMMMd().format(weather.date),
+                style: new TextStyle(color: Colors.black)),
+            Text(new DateFormat.Hm().format(weather.date),
+                style: new TextStyle(color: Colors.black)),
+          ],
+        ),
+      ),
+    );
+  }
+}
